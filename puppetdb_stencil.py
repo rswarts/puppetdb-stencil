34cb88d1a14782ddae29e700f3062cc2800c5e82
Carlo Baijens <carlo.baijens@gmail.com>
2015-07-21 17:08:22 +0200

    Add tag filtering to stencil

--- puppetdb_stencil.py
+++ puppetdb_stencil.py
@@ -14,7 +14,7 @@ import jinja2
 LOG = logging.getLogger('puppetdb_stencil')
 
 METAPARAMS = ('require', 'before', 'subscribe', 'notify', 'audit', 'loglevel',
-              'noop', 'schedule', 'stage', 'alias', 'tag')
+              'ensure', 'noop', 'schedule', 'stage', 'alias', 'tag')
 
 # Allow templates from anywhere on the filesystem
 LOADER = jinja2.FileSystemLoader(['.', '/'])
@@ -22,14 +22,20 @@ EXTENSIONS = ['jinja2.ext.with_', 'jinja2.ext.loopcontrols']
 ENVIRONMENT = jinja2.Environment(trim_blocks=True, lstrip_blocks=True,
                                  loader=LOADER, extensions=EXTENSIONS)
 
-
-def render_resources(database, resource_type, template_names):
+def render_resources(database, resource_type, template_names, query):
     """
     Render resources of the given type. They are queried from the given
     database and rendered using the first template from template_names that can
     be loaded.
     """
-    resources = database.resources(resource_type)
+    full_query = []
+    if query == "":
+        resources = database.resources(resource_type, query='["and",["~", "tag", "" ]]')
+    else:
+        for tag in query:
+            qtag = '["=", "tag", "%s" ]' % str(tag) 
+            full_query.append(qtag)
+        resources = database.resources(resource_type, query='["and", %s ]' % (", ".join(full_query)) )
     try:
         template = ENVIRONMENT.select_template(template_names)
     except jinja2.TemplatesNotFound:
@@ -49,17 +55,17 @@ def main():
     parser.add_argument('--debug', '-d', action='store_true')
     parser.add_argument('--host', '-H', default='localhost')
     parser.add_argument('--port', '-p', default='8080')
-
+    parser.add_argument('--tag', '-g', default='', nargs='+')
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.WARN)
-
+    tag=None
     database = pypuppetdb.connect(host=args.host, port=args.port)
 
     for resource_type in args.resource_types:
         templates = ['{0}.jinja2'.format(resource_type)]
         if args.templates:
             templates += args.templates
-        print(render_resources(database, resource_type, templates))
+        print(render_resources(database, resource_type, templates, args.tag))
 
 
 if __name__ == '__main__':
